bc54ac894baa70f9971d3321a9f10f2055946177
phayes <patrick.d.hayes@gmail.com>
2020-02-25 13:38:21 -0800

    Using our own keys instead of relying on ed25519_dalek crate

---
+++ src/keys.rs
@@ -0,0 +1,317 @@
+use super::Error;
+use core::iter::FromIterator;
+use curve25519_dalek::constants;
+use curve25519_dalek::edwards::{CompressedEdwardsY, EdwardsPoint};
+use curve25519_dalek::scalar::Scalar;
+use hex::{FromHex, ToHex};
+use rand::{CryptoRng, RngCore};
+use zeroize::Zeroize;
+
+/// The length of a `SecretKey`, in bytes.
+pub const SECRET_KEY_LENGTH: usize = 32;
+
+/// The length of a `PublicKey`, in bytes.
+pub const PUBLIC_KEY_LENGTH: usize = 32;
+
+#[derive(Debug)]
+pub struct SecretKey(pub(crate) [u8; SECRET_KEY_LENGTH]);
+
+/// Overwrite secret key material with null bytes when it goes out of scope.
+impl Drop for SecretKey {
+    fn drop(&mut self) {
+        self.0.zeroize();
+    }
+}
+
+/// Overwrite secret key material with null bytes when it goes out of scope.
+impl ToHex for SecretKey {
+    fn encode_hex<T: FromIterator<char>>(&self) -> T {
+        self.0.encode_hex()
+    }
+
+    fn encode_hex_upper<T: FromIterator<char>>(&self) -> T {
+        self.0.encode_hex_upper()
+    }
+}
+
+/// Overwrite secret key material with null bytes when it goes out of scope.
+impl FromHex for SecretKey {
+    type Error = Error;
+
+    fn from_hex<T: AsRef<[u8]>>(hex: T) -> Result<Self, Error> {
+        let mut bytes = Vec::<u8>::from_hex(hex).map_err(|_| Error::InvalidSecretKeyBytes)?;
+
+        let sk = Self::from_bytes(&bytes)?;
+        bytes.zeroize();
+
+        Ok(sk)
+    }
+}
+
+impl SecretKey {
+    /// Convert this secret key to a byte array.
+    #[inline]
+    pub fn to_bytes(&self) -> [u8; SECRET_KEY_LENGTH] {
+        self.0
+    }
+
+    /// View this secret key as a byte array.
+    #[inline]
+    pub fn as_bytes<'a>(&'a self) -> &'a [u8; SECRET_KEY_LENGTH] {
+        &self.0
+    }
+
+    /// Construct a `SecretKey` from a slice of bytes.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use ecies_ed25519::SecretKey;
+    /// use ecies_ed25519::SECRET_KEY_LENGTH;
+    /// use ecies_ed25519::Error;
+    ///
+    /// # fn doctest() -> Result<SecretKey, Error> {
+    /// let secret_key_bytes: [u8; SECRET_KEY_LENGTH] = [
+    ///    157, 097, 177, 157, 239, 253, 090, 096,
+    ///    186, 132, 074, 244, 146, 236, 044, 196,
+    ///    068, 073, 197, 105, 123, 050, 105, 025,
+    ///    112, 059, 172, 003, 028, 174, 127, 096, ];
+    ///
+    /// let secret_key: SecretKey = SecretKey::from_bytes(&secret_key_bytes)?;
+    /// #
+    /// # Ok(secret_key)
+    /// # }
+    /// #
+    /// # fn main() {
+    /// #     let result = doctest();
+    /// #     assert!(result.is_ok());
+    /// # }
+    /// ```
+    #[inline]
+    pub fn from_bytes(bytes: &[u8]) -> Result<SecretKey, Error> {
+        if bytes.len() != SECRET_KEY_LENGTH {
+            return Err(Error::InvalidSecretKeyBytes);
+        }
+        let mut bits: [u8; 32] = [0u8; 32];
+        bits.copy_from_slice(&bytes[..32]);
+
+        Ok(SecretKey(bits))
+    }
+
+    /// Generate a `SecretKey` from a `csprng`.
+    pub fn generate<T>(csprng: &mut T) -> SecretKey
+    where
+        T: CryptoRng + RngCore,
+    {
+        let mut sk: SecretKey = SecretKey([0u8; 32]);
+        csprng.fill_bytes(&mut sk.0);
+        sk
+    }
+}
+
+/// An ed25519 Public Key meant for use in ECIES.
+///
+/// Neither this public key (nor it's corresponding  PrivateKey) should be used for signing
+/// or in any other protocol other than ECIES.
+#[derive(Copy, Clone, Default, Eq, PartialEq)]
+pub struct PublicKey(pub(crate) CompressedEdwardsY);
+
+impl PublicKey {
+    /// Convert this public key to a byte array.
+    #[inline]
+    pub fn to_bytes(&self) -> [u8; PUBLIC_KEY_LENGTH] {
+        self.0.to_bytes()
+    }
+
+    /// View this public key as a byte array.
+    #[inline]
+    pub fn as_bytes<'a>(&'a self) -> &'a [u8; PUBLIC_KEY_LENGTH] {
+        self.0.as_bytes()
+    }
+
+    /// Construct a `PublicKey` from a slice of bytes.
+    ///
+    /// Will return None if the bytes are invalid
+    #[inline]
+    pub fn from_bytes(bytes: &[u8]) -> Result<Self, Error> {
+        let point = CompressedEdwardsY::from_slice(bytes);
+
+        if point.decompress().is_none() {
+            return Err(Error::InvalidPublicKeyBytes);
+        }
+        Ok(PublicKey(point))
+    }
+
+    /// Derive a public key from a private key
+    pub fn from_secret(sk: &SecretKey) -> Self {
+        let point = &Scalar::from_bits(sk.to_bytes()) * &constants::ED25519_BASEPOINT_TABLE;
+        PublicKey(point.compress())
+    }
+
+    /// Get the Edwards Point for this public key
+    pub fn to_point(&self) -> EdwardsPoint {
+        CompressedEdwardsY::from_slice(self.0.as_bytes())
+            .decompress()
+            .expect("ecies-ed25519: unexpect error decompressing public key")
+    }
+}
+
+impl AsRef<[u8]> for PublicKey {
+    fn as_ref(&self) -> &[u8] {
+        self.as_bytes()
+    }
+}
+
+// "serde" feature
+// ---------------
+
+#[cfg(feature = "serde")]
+use serde::{
+    de::Error as SerdeError, de::Unexpected, de::Visitor, Deserialize, Deserializer, Serialize,
+    Serializer,
+};
+
+#[cfg(feature = "serde")]
+impl Serialize for SecretKey {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        if serializer.is_human_readable() {
+            let encoded: Vec<char> = self.encode_hex();
+            let mut encoded: String = encoded.into_iter().collect();
+            let result = serializer.serialize_str(&encoded);
+
+            encoded.zeroize();
+
+            result
+        } else {
+            serializer.serialize_bytes(self.as_bytes())
+        }
+    }
+}
+
+#[cfg(feature = "serde")]
+impl<'d> Deserialize<'d> for SecretKey {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'d>,
+    {
+        struct SecretKeyVisitor;
+
+        impl<'d> Visitor<'d> for SecretKeyVisitor {
+            type Value = SecretKey;
+
+            fn expecting(&self, formatter: &mut ::core::fmt::Formatter) -> ::core::fmt::Result {
+                formatter.write_str("An ecies-ed25519 secret key as 32 bytes.")
+            }
+
+            fn visit_str<E>(self, input: &str) -> Result<SecretKey, E>
+            where
+                E: SerdeError,
+            {
+                let mut bytes = hex::decode(input).or(Err(SerdeError::invalid_value(
+                    Unexpected::Other("invalid hex"),
+                    &self,
+                )))?;
+                let sk = SecretKey::from_bytes(&bytes)
+                    .or(Err(SerdeError::invalid_length(bytes.len(), &self)))?;
+                bytes.zeroize();
+                Ok(sk)
+            }
+
+            fn visit_bytes<E>(self, bytes: &[u8]) -> Result<SecretKey, E>
+            where
+                E: SerdeError,
+            {
+                // If it's 64 bytes, then it must be hex encoded
+                if bytes.len() == 64 {
+                    let mut bytes = hex::decode(bytes).or(Err(SerdeError::invalid_value(
+                        Unexpected::Other("neither valid hex nor a 32 byte array"),
+                        &self,
+                    )))?;
+                    let sk = SecretKey::from_bytes(&bytes).unwrap();
+                    bytes.zeroize();
+                    Ok(sk)
+                } else {
+                    SecretKey::from_bytes(bytes)
+                        .or(Err(SerdeError::invalid_length(bytes.len(), &self)))
+                }
+            }
+        }
+        deserializer.deserialize_bytes(SecretKeyVisitor)
+    }
+}
+
+#[cfg(feature = "serde")]
+impl Serialize for PublicKey {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        if serializer.is_human_readable() {
+            let encoded: Vec<char> = self.encode_hex();
+            let mut encoded: String = encoded.into_iter().collect();
+            let result = serializer.serialize_str(&encoded);
+
+            encoded.zeroize();
+
+            result
+        } else {
+            serializer.serialize_bytes(self.as_bytes())
+        }
+    }
+}
+
+#[cfg(feature = "serde")]
+impl<'d> Deserialize<'d> for PublicKey {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'d>,
+    {
+        struct PublicKeyVisitor;
+
+        impl<'d> Visitor<'d> for PublicKeyVisitor {
+            type Value = PublicKey;
+
+            fn expecting(&self, formatter: &mut ::core::fmt::Formatter) -> ::core::fmt::Result {
+                formatter.write_str("An ecies-ed25519 public key as 32 bytes.")
+            }
+
+            fn visit_str<E>(self, input: &str) -> Result<PublicKey, E>
+            where
+                E: SerdeError,
+            {
+                let mut bytes = hex::decode(input).or(Err(SerdeError::invalid_value(
+                    Unexpected::Other("invalid hex"),
+                    &self,
+                )))?;
+                let pk = PublicKey::from_bytes(&bytes)
+                    .or(Err(SerdeError::invalid_length(bytes.len(), &self)))?;
+                bytes.zeroize();
+                Ok(pk)
+            }
+
+            fn visit_bytes<E>(self, bytes: &[u8]) -> Result<PublicKey, E>
+            where
+                E: SerdeError,
+            {
+                // If it's 64 bytes, then it must be hex encoded
+                if bytes.len() == 64 {
+                    let mut bytes = hex::decode(bytes).or(Err(SerdeError::invalid_value(
+                        Unexpected::Other("neither valid hex nor a 32 byte array"),
+                        &self,
+                    )))?;
+                    let pk = PublicKey::from_bytes(&bytes)
+                        .or(Err(SerdeError::invalid_length(bytes.len(), &self)))?;
+                    bytes.zeroize();
+                    Ok(pk)
+                } else {
+                    PublicKey::from_bytes(bytes)
+                        .or(Err(SerdeError::invalid_length(bytes.len(), &self)))
+                }
+            }
+        }
+        deserializer.deserialize_bytes(PublicKeyVisitor)
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -37,14 +37,12 @@
 //! let decrypted = ecies_ed25519::decrypt(&secret, &encrypted);
 //!```
 
-use curve25519_dalek::constants;
-use curve25519_dalek::edwards::{CompressedEdwardsY, EdwardsPoint};
 use curve25519_dalek::scalar::Scalar;
-use ed25519_dalek::PublicKey as EdPublicKey;
-pub use ed25519_dalek::SecretKey;
-use ed25519_dalek::PUBLIC_KEY_LENGTH;
 use failure::Fail;
 use rand::{CryptoRng, RngCore};
+
+mod keys;
+pub use keys::*;
 
 #[cfg(feature = "ring")]
 mod ring_backend;
@@ -75,61 +73,9 @@ const AES_IV_LENGTH: usize = 12;
 type AesKey = [u8; 32];
 type SharedSecret = [u8; 32];
 
-/// An ed25519 Public Key meant for use in ECIES.
-///
-/// Neither this public key (nor it's corresponding  PrivateKey) should be used for signing
-/// or in any other protocol other than ECIES.
-#[derive(Debug, Clone)]
-pub struct PublicKey(EdPublicKey);
-
-impl PublicKey {
-    /// Convert this public key to a byte array.
-    #[inline]
-    pub fn to_bytes(&self) -> [u8; PUBLIC_KEY_LENGTH] {
-        self.0.to_bytes()
-    }
-
-    /// View this public key as a byte array.
-    #[inline]
-    pub fn as_bytes<'a>(&'a self) -> &'a [u8; PUBLIC_KEY_LENGTH] {
-        self.0.as_bytes()
-    }
-
-    /// Construct a `PublicKey` from a slice of bytes.
-    ///
-    /// Will return None if the bytes are invalid
-    #[inline]
-    pub fn from_bytes(bytes: &[u8]) -> Result<Self, Error> {
-        let public = EdPublicKey::from_bytes(bytes).map_err(|_| Error::InvalidPublicKeyBytes)?;
-
-        Ok(PublicKey(public))
-    }
-
-    /// Derive a public key from a private key
-    pub fn from_secret(sk: &SecretKey) -> Self {
-        let point = &Scalar::from_bits(sk.to_bytes()) * &constants::ED25519_BASEPOINT_TABLE;
-        let public = EdPublicKey::from_bytes(&point.compress().to_bytes())
-            .expect("ecies-ed25519: unexpect error generating public key from private key");
-        PublicKey(public)
-    }
-
-    /// Get the Edwards Point for this public key
-    pub fn to_point(&self) -> EdwardsPoint {
-        CompressedEdwardsY::from_slice(self.0.as_bytes())
-            .decompress()
-            .expect("ecies-ed25519: unexpect error decompressing public key")
-    }
-}
-
-impl AsRef<[u8]> for PublicKey {
-    fn as_ref(&self) -> &[u8] {
-        self.as_bytes()
-    }
-}
-
 /// Generate a keypair, ready for use in ECIES
 pub fn generate_keypair<R: CryptoRng + RngCore>(rng: &mut R) -> (SecretKey, PublicKey) {
-    let ed25519_dalek::Keypair { public: _, secret } = ed25519_dalek::Keypair::generate(rng);
+    let secret = SecretKey::generate(rng);
     let public = PublicKey::from_secret(&secret);
     (secret, public)
 }
@@ -226,6 +172,10 @@ pub enum Error {
     /// Invalid public key bytes
     #[fail(display = "ecies-rd25519: invalid public key bytes")]
     InvalidPublicKeyBytes,
+
+    /// Invalid secret key bytes
+    #[fail(display = "ecies-rd25519: invalid secret key bytes")]
+    InvalidSecretKeyBytes,
 }
 
 #[cfg(test)]
@@ -338,5 +288,21 @@ pub mod tests {
         let decrypted = decrypt(&peer_sk, &known_encrypted).unwrap();
 
         assert_eq!(plaintext, decrypted.as_slice());
+    }
+
+    #[cfg(feature = "serde")]
+    #[test]
+    fn test_serde_json() {
+        let mut test_rng = rand::rngs::StdRng::from_seed([0u8; 32]);
+        let (secret, public) = generate_keypair(&mut test_rng);
+
+        let serialized_secret = serde_json::to_string(&secret).unwrap();
+        let serialized_public = serde_json::to_string(&public).unwrap();
+
+        let deserialized_secret: SecretKey = serde_json::from_str(&serialized_secret).unwrap();
+        let deserialized_public: PublicKey = serde_json::from_str(&serialized_public).unwrap();
+
+        assert_eq!(secret.to_bytes(), deserialized_secret.to_bytes());
+        assert_eq!(public.as_bytes(), deserialized_public.as_bytes());
     }
 }
